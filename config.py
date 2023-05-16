f622641024b0e646071acb231f866837f521f926
Dagem Amare <dagemamare@gmail.com>
2023-05-16 22:44:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,11 +8,11 @@ USERS = {
 
 MODES = {
     # Classic mode, easy to detect
-    "classic": True,
+    "classic": False,
 
     # Makes the proxy harder to detect
     # Can be incompatible with very old clients
-    "secure": False,
+    "secure": True,
 
     # Makes the proxy even more hard to detect
     # Can be incompatible with old clients
